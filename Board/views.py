6bf73d8252dd1538ece57d3162ad263a2eac9d7b
Perryanto Hartono <phar2413@uni.sydney.edu.au>
2019-11-29 23:20:30 +1100

    Removed menu, it will not be required

--- Board/views.py
+++ Board/views.py
@@ -5,6 +5,7 @@ def mainPage(request):
     #to_message_list = Message.objects.filter(to_user=username).all()
     #post = Post.objects.get(pk=postId)
     context = {
+        'title': "Home Whiteboard by 5hwb",
     #    'to_message_list': to_message_list,
     #    'username': username,
     }
