9b36c39298e29094c9828a4b7a684d78337ee655
Perryanto Hartono <phar2413@uni.sydney.edu.au>
2019-11-30 00:31:39 +1100

    New posts can be added now

--- Board/views.py
+++ Board/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse_lazy
+from .models import Post
 
 # Create your views here.
 def mainPage(request):
@@ -12,7 +15,16 @@ def mainPage(request):
     return render(request, 'home.html', context)
 
 def addPost(request):
-    context = {
-        'title': "Home Whiteboard by 5hwb",
-    }
-    return render(request, 'add_post.html', context)
+    if request.method == 'POST':
+        text = request.POST.get("postText","")
+        c = Post(text=text)
+        c.save()
+        return HttpResponseRedirect(reverse_lazy('mainPage'))
+
+    else:
+        post_list = Post.objects.all()
+        context = {
+            'title': "Home Whiteboard by 5hwb",
+            'post_list': post_list,
+        }
+        return render(request, 'add_post.html', context)
