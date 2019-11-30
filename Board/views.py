eb5c5be9418b1e4715618c171bcab1c6b804ef12
Perryanto Hartono <phar2413@uni.sydney.edu.au>
2019-11-30 13:55:10 +1100

    Change title of page

--- Board/views.py
+++ Board/views.py
@@ -15,7 +15,7 @@ def main_page(request):
     post_list = Post.objects.all()
     split_post_list = list(chunks(post_list, 2))
     context = {
-        'title': "Home Whiteboard by 5hwb",
+        'title': "Big Text Notes by 5hwb",
         'post_list': post_list,
         'split_post_list': split_post_list,
     }
@@ -31,7 +31,7 @@ def add_post(request):
     else:
         post_list = Post.objects.all()
         context = {
-            'title': "Add a new post - Home Whiteboard by 5hwb",
+            'title': "Add a new post - Big Text Notes by 5hwb",
             'post_list': post_list,
         }
         return render(request, 'add_post.html', context)
@@ -53,7 +53,7 @@ def edit_post(request, post_pk):
         post_list = Post.objects.all()
         existing_post = Post.objects.get(id=post_pk)
         context = {
-            'title': "Edit post - Home Whiteboard by 5hwb",
+            'title': "Edit post - Big Text Notes by 5hwb",
             'post_list': post_list,
             'existing_post': existing_post,
         }
